2b82b813d9639f650896ff11aca0ae2425249e6a
Uly Chen <uly.chen@gmail.com>
2019-04-07 16:11:45 +0900

    3333

--- dotnet-core-api/Controllers/ValuesController.cs
+++ dotnet-core-api/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace dotnet_core_api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value111111", "value22222" };
+            return new string[] { "value333333", "value22222" };
         }
 
         // GET api/values/5
